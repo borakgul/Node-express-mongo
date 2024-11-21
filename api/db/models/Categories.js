4a7adde50b2733f89aabd0586c5e07b5d8c3fc53
Bora AKGUL <borakgul1@gmail.com>
2024-11-21 17:04:23 +0300

    Categories CRUD Operation and error handling implementation. #1

---
+++ api/db/models/Categories.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    name :{type:String,required:true},
+    is_active:{type:Boolean,default:true,},
+    created_by: {type: mongoose.SchemaTypes.ObjectId}   
+},
+{
+    versionKey: false,
+         
+     timestamps:{// timestamps:true aynı şey
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    } //veri veya şema eklerken bu alanlar otomatik olarak alınır. Ek olarak bellirtmek gerekmez.
+} 
+);
+class Categories extends mongoose.model{
+
+}
+
+schema.loadClass(Categories);
+module.exports = mongoose.model('categories',schema); //Users ismiyle schema'yı export et.
+   
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,9 +1,80 @@
 var express = require('express');
 var router = express.Router();
+const Categories = require('../db/models/Categories');
+const Response = require('../lib/Response');
+const CustomError = require("../lib/Error");
+const Enum = require("../config/Enum");
+const { name } = require('ejs');
 
-router.get('/', function(req, res, next) {  //http://localhost:3000/api/categories 
-  res.json({ success: true, message: "Categories" });
-}   //http://localhost:3000/users/ geldiğinde 'respond with a resource' yazdır. 
+router.get('/', async(req, res, next) => {  //http://localhost:3000/api/categories 
+  try {
+      let categories = await Categories.find({});  
+       //tüm kategorileri getir.
+      res.json(Response.successResponse(categories)); // data verisini yollayacağız burada categories olacak.
+  }
+    catch (err) {
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(Response.errorResponse(err));
+    }
+});
+
+router.post("/add",async(req,res)=>{
+    let body = req.body;
+    try{
+
+        if (!body.name) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST,"Validation Error","Name field must be filled.");
+
+        let category = new Categories({
+            name:body.name,
+            isActive:true,
+            created_by:req.user?._id
+
+
+        });
+        await category.save();
+
+        res.json(Response.successResponse({success: true})) ;
+
+    }catch (err){
+
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(errorResponse);
+    }
+
+    });
     
-    );
+    router.post("/update",async(req,res)=>{
+        let body = req.body;
+        try{
+            if(!body._id) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST,"Validation Error","_id field must be filled.");
+            
+            let updates = {};
+
+            if (body.name) updates.name = body.name;
+            if (typeof body.is_active === "boolean") updates.is_active = body.is_active;
+            
+            await Categories.updateOne({_id:body._id},updates);
+            res.json(Response.successResponse({success: true})) ;
+        }catch(err) {
+            let errorResponse = Response.errorResponse(err);
+            res.status(errorResponse.code).json(errorResponse);
+        }
+    });
+    router.post("/delete",async(req,res)=>{
+        let body = req.body
+
+        try {
+            if(!body._id) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST,"Validation Error","_id field must be filled.");
+
+            await Categories.deleteOne({_id:body._id});
+
+            res.json(Response.successResponse({success: true})) ;
+
+        } catch (err) {
+
+            let errorResponse = Response.errorResponse(err);
+
+            res.status(errorResponse.code).json(errorResponse);
+        }
+    })
     module.exports = router; //router'ı export et.
