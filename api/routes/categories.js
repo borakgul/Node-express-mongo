8ad9b60f423158da4acd659c7ade85fdfad8bef5
Bora AKGUL <borakgul1@gmail.com>
2024-12-17 16:48:34 +0300

    #16 Implement event emitter

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -9,6 +9,7 @@ const logger = require('../lib/logger/loggerclas');
 const auth = require("../lib/auth")();
 const config = require("../config");
 const i18n = new(require("../lib/i18n"))(config.DEFAULT_LANG);
+const emitter = require("../lib/Emitter");
 
 router.all("*", auth.authenticate(), (req, res, next) => {
     next();
@@ -42,6 +43,7 @@ router.post("/add",auth.checkRoles("category_add") , async(req,res)=>{
 
         AuditLogs.info(req.user?.email, "Categories", "Add", category);
         logger.info(req.user?.email, "Categories", "Add", category);
+        emitter.getEmitter("notifications").emit("messages",{message: category.name + "is added"});
 
         res.json(Response.successResponse({success: true})) ;
 
